95e9a0decfe27878617a5662ea6220d410f57790
Sérgio Miguel Fernandes <sergio.fernandes@ist.utl.pt>
2013-06-06 14:14:31 +0100

    Create the plugin ff2
      * With support in RadarGunDataGridConfig to override FF defaults, such as
        hazelcast config.

---
+++ plugins/ff2/src/main/java/org/radargun/cachewrappers/FFWrapper.java
@@ -0,0 +1,223 @@
+package org.radargun.cachewrappers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.radargun.CacheWrapper;
+import org.radargun.utils.TypedProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import pt.ist.fenixframework.FenixFramework;
+import pt.ist.fenixframework.backend.jvstm.datagrid.JvstmDataGridBackEnd;
+import pt.ist.fenixframework.backend.jvstm.datagrid.radargun.DataGridDecorator;
+import pt.ist.fenixframework.backend.jvstm.datagrid.radargun.RadarGunDataGridConfig;
+
+public class FFWrapper implements CacheWrapper {
+
+    private static final Logger logger = LoggerFactory.getLogger(FFWrapper.class);
+
+    public static final String FF_CONFIG_CLASS = "pt.ist.fenixframework.backend.jvstm.datagrid.radargun.RadarGunDataGridConfig";
+    public static final String DATA_GRID_CONFIG_FILE = "dataGridConfigFile";
+
+    public static final String FF_CONFIG_OPTIONS_FILE = "rg-fenix-framework-config-options.properties";
+
+    protected static JvstmDataGridBackEnd backEnd;
+
+    private String info;
+
+    private static Properties loadFFConfigOptions() {
+        Properties props = new Properties();
+
+        InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(FF_CONFIG_OPTIONS_FILE);
+        if (in == null) {
+            logger.info("Resource '" + FF_CONFIG_OPTIONS_FILE + "' not found");
+            return null;
+        }
+
+        logger.info("Found fenix framework config options in '" + FF_CONFIG_OPTIONS_FILE + "'.");
+
+        // add the new properties
+        try {
+            props.load(in);
+        } catch (IOException e) {
+            logger.warn("Error reading " + FF_CONFIG_OPTIONS_FILE, e);
+            return null;
+        } finally {
+            try {
+                in.close();
+            } catch (Throwable ignore) {
+            }
+        }
+        return props;
+    }
+
+    @Override
+    public void setUp(String config, boolean isLocal, int nodeIndex, TypedProperties confAttributes) throws Exception {
+        logger.info("config={}, isLocal={}, nodeIndex={}, confAttributes={}", config, isLocal, nodeIndex, confAttributes);
+        this.info = confAttributes.getProperty(DataGridDecorator.DECORATE_KEY);
+
+        RadarGunDataGridConfig ffConfig = new RadarGunDataGridConfig(config, isLocal, nodeIndex, confAttributes);
+
+        Properties ffProperties = loadFFConfigOptions();
+        if (ffProperties != null) {
+            logger.info("Setting extra config properties for Fenix Framework.");
+            ffConfig.setExtraProperties(ffProperties);
+        }
+        logger.info("Will initialize Fenix Framework with RadarGunDataGridConfig");
+        FenixFramework.initialize(ffConfig);
+
+        logger.info("FF STARTED=" + FenixFramework.isInitialized());
+        backEnd = JvstmDataGridBackEnd.getInstance();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        FenixFramework.shutdown();
+    }
+
+    @Override
+    public boolean isRunning() {
+        return FenixFramework.isInitialized();
+    }
+
+    @Override
+    public void put(String bucket, Object key, Object value) throws Exception {
+        logger.debug("PUT: key={}, value={}", key, value);
+        boolean autoCommit = false;
+
+        if (FenixFramework.getTransaction() == null) {
+            startTransaction();
+            autoCommit = true;
+        }
+
+        try {
+            backEnd.vboxFromId(key.toString()).put(value);
+        } finally {
+            if (autoCommit) {
+                endTransaction(true);
+            }
+        }
+    }
+
+    @Override
+    public Object get(String bucket, Object key) throws Exception {
+        logger.debug("GET: key={}", key);
+        boolean autoCommit = false;
+
+        if (FenixFramework.getTransaction() == null) {
+            startTransaction();
+            autoCommit = true;
+        }
+
+        try {
+            return backEnd.vboxFromId(key.toString()).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+            return null; // the compiler requires it
+        } finally {
+            if (autoCommit) {
+                endTransaction(true);
+            }
+        }
+    }
+
+    @Override
+    public Object remove(String bucket, Object key) throws Exception {
+        logger.debug("REMOVE: key={}", key);
+        boolean autoCommit = false;
+
+        if (FenixFramework.getTransaction() == null) {
+            startTransaction();
+            autoCommit = true;
+        }
+
+        try {
+            Object previous = get(bucket, key);
+            put(bucket, key, null);
+            return previous;
+        } finally {
+            if (autoCommit) {
+                endTransaction(true);
+            }
+        }
+    }
+
+    @Override
+    public void empty() throws Exception {
+        logger.warn("Fenix Framework never forgets...");
+//        logger.debug("GET: EMPTY");
+//        boolean autoCommit = false;
+//
+//        if (FenixFramework.getTransaction() == null) {
+//            startTransaction();
+//            autoCommit = true;
+//        }
+//
+//        try {
+//            backend.deleteAll();
+//            // TODO Auto-generated method stub
+//            throw new UnsupportedOperationException(
+//                    "not yet implemented. should remove/clear all elements from the underlying cache");
+//        } finally {
+//            if (autoCommit) {
+//                endTransaction(true);
+//            }
+//        }
+    }
+
+    @Override
+    public int getNumMembers() {
+        return backEnd.getNumMembers();
+    }
+
+    @Override
+    public String getInfo() {
+        return "FFWrapper using " + this.info;
+    }
+
+    @Override
+    public Object getReplicatedData(String bucket, String key) throws Exception {
+        return get(bucket, key);
+    }
+
+    @Override
+    public boolean isTransactional(String bucket) {
+        return true;
+    }
+
+    @Override
+    public void startTransaction() {
+        try {
+            FenixFramework.getTransactionManager().begin();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void endTransaction(boolean successful) {
+        try {
+            if (successful) {
+                FenixFramework.getTransactionManager().commit();
+            } else {
+                FenixFramework.getTransactionManager().rollback();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public int getLocalSize() {
+        return -1;
+    }
+
+    @Override
+    public int getTotalSize() {
+        return -1;
+    }
+
+}
---
+++ plugins/ff2/src/main/java/pt/ist/fenixframework/backend/jvstm/datagrid/radargun/DataGridDecorator.java
@@ -0,0 +1,133 @@
+package pt.ist.fenixframework.backend.jvstm.datagrid.radargun;
+
+import java.net.URLClassLoader;
+
+import org.radargun.CacheWrapper;
+import org.radargun.utils.TypedProperties;
+import org.radargun.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import pt.ist.fenixframework.backend.jvstm.datagrid.DataGrid;
+import pt.ist.fenixframework.backend.jvstm.datagrid.JvstmDataGridConfig;
+import pt.ist.fenixframework.backend.jvstm.repository.PersistenceException;
+
+public class DataGridDecorator implements DataGrid {
+
+    private static final Logger logger = LoggerFactory.getLogger(DataGridDecorator.class);
+
+    public static final String DECORATE_KEY = "decorate";
+
+    private static final String MISSING_PRODUCT_PLUGIN =
+            "Please provide the name of a valid plugin to decorate using the 'decorated=' property";
+
+    private static final String UNDERLYING_WRAPPER_EXCEPTION = "Exception in underlying cache wrapper";
+
+    private CacheWrapper cacheWrapper;
+    private boolean inTransaction = false;
+
+    @Override
+    public void init(JvstmDataGridConfig dataGridConfig) {
+        RadarGunDataGridConfig radarGunConfig = (RadarGunDataGridConfig) dataGridConfig;
+
+        TypedProperties confAttributes = radarGunConfig.getConfAttributes();
+        String product = (String) confAttributes.remove(DECORATE_KEY);
+
+        if (product == null) {
+            throw new IllegalArgumentException(MISSING_PRODUCT_PLUGIN);
+        }
+
+        try {
+            this.cacheWrapper = getCacheWrapper(product);
+        } catch (Exception e) {
+            logger.error("Could not obtain decorated cache wrapper", e);
+            throw new IllegalArgumentException(MISSING_PRODUCT_PLUGIN, e);
+        }
+
+        String config = radarGunConfig.getConfig();
+        boolean isLocal = radarGunConfig.getIsLocal();
+        int nodeIndex = radarGunConfig.getNodeIndex();
+
+        logger.info("config={}, isLocal={}, nodeIndex={}, confAttributes={}", config, isLocal, nodeIndex, confAttributes);
+
+        try {
+            this.cacheWrapper.setUp(config, isLocal, nodeIndex, confAttributes);
+        } catch (Exception e) {
+            logger.error("Could not setup decorated cache wrapper", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void stop() {
+        try {
+            this.cacheWrapper.tearDown();
+        } catch (Exception e) {
+            logger.warn(UNDERLYING_WRAPPER_EXCEPTION);
+            throw new PersistenceException(UNDERLYING_WRAPPER_EXCEPTION, e);
+        }
+    }
+
+    @Override
+    public Object get(Object key) {
+        try {
+            return this.cacheWrapper.get(null, key);
+        } catch (Exception e) {
+            logger.warn(UNDERLYING_WRAPPER_EXCEPTION);
+            throw new PersistenceException(UNDERLYING_WRAPPER_EXCEPTION, e);
+        }
+    }
+
+    @Override
+    public void put(Object key, Object value) {
+        try {
+            this.cacheWrapper.put(null, key, value);
+        } catch (Exception e) {
+            logger.warn(UNDERLYING_WRAPPER_EXCEPTION);
+            throw new PersistenceException(UNDERLYING_WRAPPER_EXCEPTION, e);
+        }
+    }
+
+    @Override
+    public void beginTransaction() {
+        try {
+            this.cacheWrapper.startTransaction();
+            this.inTransaction = true;
+        } catch (Exception e) {
+            this.inTransaction = false;
+        }
+    }
+
+    @Override
+    public void commitTransaction() {
+        try {
+            this.cacheWrapper.endTransaction(true);
+        } finally {
+            this.inTransaction = false;
+        }
+    }
+
+    @Override
+    public void rollbackTransaction() {
+        try {
+            this.cacheWrapper.endTransaction(false);
+        } finally {
+            this.inTransaction = false;
+        }
+    }
+
+    @Override
+    public boolean inTransaction() {
+        return this.inTransaction;
+    }
+
+    // Adapted from LocalBenchmark.getCacheWrapper
+    private CacheWrapper getCacheWrapper(String product) throws Exception {
+        String fqnClass = Utils.getCacheWrapperFqnClass(product);
+
+        URLClassLoader loader = Utils.buildProductSpecificClassLoader(product, getClass().getClassLoader());
+        Thread.currentThread().setContextClassLoader(loader);
+        return (CacheWrapper) loader.loadClass(fqnClass).newInstance();
+    }
+
+}
